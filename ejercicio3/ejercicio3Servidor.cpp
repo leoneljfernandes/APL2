8bc62446070ae4abef1824ce954da2784034f700
Leonel <leo99.fernandes@gmail.com>
2025-05-27 18:18:14 +0000

    Fix Ejercicio 3

--- ejercicio3/ejercicio3Servidor.cpp
+++ ejercicio3/ejercicio3Servidor.cpp
@@ -12,7 +12,7 @@
 #include <algorithm>
 
 #define NOMBRE_FIFO "miFifo"
-#define BUFFER_SIZE 1024
+#define TIMEOUT_SEGUNDOS 5  // Tiempo de espera para considerar inactividad
 
 using namespace std;
 
@@ -30,97 +30,6 @@ void mostrarAyuda() {
     cout << "  ./ejercicio3Servidor -h     # Muestra ayuda" << endl;
 }
 
-
-/*
-int main(int argc, char *argv[]) {
-
-    if (argc > 1 && strcmp(argv[1], "-h") == 0) {
-        mostrarAyuda();
-        return 0;
-    }
-
-    // Limpiamos la cola de impresion y resultados previos
-    unlink(NOMBRE_FIFO);
-
-    string LOG_TEMPORAL = "/tmp/impresiones.log"; // creo log
-    // Limpiamos el archivo de log
-    try{
-        ofstream log(LOG_TEMPORAL, ios::trunc);
-        log.close();
-    }catch (const exception& e) {
-        cout << "Error al limpiar el archivo de log: " << e.what() << endl;
-    }
-    // creo el archivo fifo
-    mkfifo(NOMBRE_FIFO, 0666);
-
-    // Leemos el fifo de uno a la vez
-    while(true){
-        ifstream fifo(NOMBRE_FIFO);
-        string buffer;
-
-        while(getline(fifo, buffer)){
-
-            if(!buffer.empty()){
-                size_t sep = buffer.find(':');
-                if(sep != string:: npos){
-                    string pidCliente = buffer.substr(0, sep);
-                    string archivo = buffer.substr(sep + 1);
-
-                    //simular impresion
-                    cout << "Imprimiendo archivo: " << archivo << endl;
-                    sleep(2); // Simulamos un tiempo de impresión de 2 segundos
-
-                    // abro archivo y log en append para escribir
-                    ifstream archivoImpreso(archivo);
-                    ofstream log(LOG_TEMPORAL, ios::app);
-
-                    if(archivoImpreso && log){
-                        // Obtener el tiempo actual
-                        auto ahora = std::chrono::system_clock::now();
-                        std::time_t tiempo_actual = std::chrono::system_clock::to_time_t(ahora);
-                        // Convertir a estructura tm para formateo
-                        std::tm* tiempo_local = std::localtime(&tiempo_actual);
-                        // Crear un stringstream para formatear
-                        std::ostringstream oss;
-
-                        // Formatear según lo solicitado
-                        oss << "PID {" << pidCliente << "} imprimió el archivo {" << archivo
-                            << "} el día {" << std::put_time(tiempo_local, "%d/%m/%Y")
-                            << "} a las {" << std::put_time(tiempo_local, "%H:%M:%S") << "}";
-
-                        log << oss.str() << endl;
-                        log << archivoImpreso.rdbuf() << endl << endl;
-                        
-                        string fifoRespuestaPrivado = "/tmp/FIFO_" + pidCliente;
-                        ofstream respuesta(fifoRespuestaPrivado);
-                        respuesta << "OK" << endl;
-                        respuesta.close();
-                        unlink(fifoRespuestaPrivado.c_str()); // Eliminar FIFO privado
-
-                        
-
-                    }else{
-                        // Enviar mensaje de error al cliente
-                        string fifoCliente = "/tmp/FIFO_" + pidCliente;
-                        ofstream respuesta(fifoCliente);
-                        respuesta << "Error al procesar el archivo" << endl;
-                        respuesta.close();
-                        unlink(fifoCliente.c_str()); // Eliminar FIFO privado
-                    }
-
-                }
-            }
-
-        }
-        //cierro fifo publico
-        fifo.clear();
-        fifo.close();
-
-    }
-    
-
-}
-*/
 
 int main(int argc, char *argv[]) {
     if (argc > 1 && strcmp(argv[1], "-h") == 0) {
@@ -148,69 +57,66 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_leidos;
+        // Convertir descriptor a ifstream
+    FILE* fifo_file = fdopen(fd, "r");
+    if (!fifo_file) {
+        perror("Error al crear FILE*");
+        close(fd);
+        return 1;
+    }
+
+    cout << "Servidor iniciado (se cerrará después de " << TIMEOUT_SEGUNDOS 
+         << " segundos de inactividad)" << endl;
+
+    time_t ultima_actividad = time(nullptr);
+    string linea;
+
 
     cout << "Servidor de impresión iniciado. Esperando trabajos..." << endl;
 
     while (true) {
-        bytes_leidos = read(fd, buffer, BUFFER_SIZE - 1);
-        
-        if (bytes_leidos > 0) {
-            buffer[bytes_leidos] = '\0';
-            string mensaje(buffer);
+        char buffer[1024];
+        if (fgets(buffer, sizeof(buffer), fifo_file)) {
+            ultima_actividad = time(nullptr);
+            linea = buffer;
             
-            size_t sep = mensaje.find(':');
+            size_t sep = linea.find(':');
             if (sep != string::npos) {
-                string pidCliente = mensaje.substr(0, sep);
-                string archivo = mensaje.substr(sep + 1);
-                
-                // Eliminar posibles saltos de línea
+                string pidCliente = linea.substr(0, sep);
+                string archivo = linea.substr(sep + 1);
                 archivo.erase(remove(archivo.begin(), archivo.end(), '\n'), archivo.end());
 
-                cout << "Imprimiendo archivo: " << archivo << " para cliente " << pidCliente << endl;
-                sleep(2); // Simular tiempo de impresión
+                cout << "Procesando: " << archivo << endl;
+                sleep(2); // Simular impresión
 
-                // Procesar archivo
-                ifstream archivoImpreso(archivo);
+                // Registrar en log
                 ofstream log(LOG_TEMPORAL, ios::app);
-
-                if (archivoImpreso && log) {
+                ifstream archivo_leer(archivo);
+                if (log && archivo_leer) {
                     auto ahora = chrono::system_clock::now();
-                    time_t tiempo_actual = chrono::system_clock::to_time_t(ahora);
-                    tm* tiempo_local = localtime(&tiempo_actual);
-                    
-                    ostringstream oss;
-                    oss << "PID {" << pidCliente << "} imprimió el archivo {" << archivo
-                        << "} el día {" << put_time(tiempo_local, "%d/%m/%Y")
-                        << "} a las {" << put_time(tiempo_local, "%H:%M:%S") << "}";
-
-                    log << oss.str() << endl;
-                    log << archivoImpreso.rdbuf() << endl << endl;
-                    
-                    // Responder al cliente
-                    string fifoRespuesta = "/tmp/FIFO_" + pidCliente;
-                    ofstream respuesta(fifoRespuesta);
-                    if (respuesta) {
-                        respuesta << "OK" << endl;
-                    }
-                    respuesta.close();
-                } else {
-                    string fifoRespuesta = "/tmp/FIFO_" + pidCliente;
-                    ofstream respuesta(fifoRespuesta);
-                    if (respuesta) {
-                        respuesta << "Error al procesar el archivo" << endl;
-                    }
-                    respuesta.close();
+                    time_t t = chrono::system_clock::to_time_t(ahora);
+                    log << "PID " << pidCliente << " | Archivo: " << archivo 
+                        << " | Fecha: " << put_time(localtime(&t), "%F %T") << "\n"
+                        << archivo_leer.rdbuf() << "\n\n";
                 }
-            }
-        } else if (bytes_leidos == -1) {
-            perror("Error al leer del FIFO");
-            break;
-        }
-    }
 
+                // Responder al cliente
+                ofstream respuesta("/tmp/FIFO_" + pidCliente);
+                if (respuesta) respuesta << "OK" << endl;
+            }
+        } else {
+            // Verificar timeout
+            if (difftime(time(nullptr), ultima_actividad) > TIMEOUT_SEGUNDOS) {
+                cout << "Timeout de inactividad. Cerrando servidor..." << endl;
+                break;
+            }
+            usleep(100000); // Esperar 100ms antes de reintentar
+        }
+    // Limpieza
+    fclose(fifo_file);
     close(fd);
     unlink(NOMBRE_FIFO);
+    cout << "Servidor cerrado correctamente" << endl;
     return 0;
+    }
 }

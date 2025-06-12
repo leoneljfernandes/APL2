6a0ed1af7b9b2e26e8677a1ada18b57fda1cee5e
Leonel <leo99.fernandes@gmail.com>
2025-06-12 01:56:20 +0000

    Fix Ej3

--- ejercicio3/ejercicio3Cliente.cpp
+++ ejercicio3/ejercicio3Cliente.cpp
@@ -16,36 +16,20 @@ void mostrarAyuda() {
     cout << "  Este programa simula una cola de impresion FIFO." << endl;
     cout << "  Este es el programa cliente el cual pondra en cola los archivos que indique." << endl;
     cout << "Opciones:" << endl;
-    cout << "  -h                               Muestra este mensaje de ayuda" << endl;
-    cout << " -i / --impresiones <num>          Cantidad de archivos a imprimir (Requerido). Valor entero positivo." << endl;
-    cout << " -a / --archivo <ruta archivo>     Archivo a imprimir. (Requerido)." << endl;
+    cout << "  -h                               Muestra este mensaje de ayuda" << endl;    cout << " -a / --archivo <ruta archivo>     Archivo a imprimir. (Requerido)." << endl;
     cout << endl;
     cout << "Ejemplos:" << endl;
-    cout << "  ./ejercicio3Cliente -i 2 -a ./archivos/prueba         # Ejecución normal" << endl;
-    cout << "  ./ejercicio3Cliente -h                                # Muestra ayuda" << endl;
+    cout << "  ./ejercicio3Cliente -a ./archivos/prueba         # Ejecución normal" << endl;
+    cout << "  ./ejercicio3Cliente -h                           # Muestra ayuda" << endl;
 }
 
 void verificarParametros(int argc, char *argv[]) {
-    bool tieneImpresiones = false;
     bool tieneArchivo = false;
 
     for(int i = 1; i < argc; i++) {  // Empezar en 1 para omitir el nombre del programa
         string arg = argv[i];
         
-        if (arg == "-i" || arg == "--impresiones") {
-            if (i + 1 >= argc) {
-                throw runtime_error("Falta la cantidad de impresiones después de " + arg);
-            }
-            
-            string valor = argv[i+1];
-            if (valor.find_first_not_of("0123456789") != string::npos) {
-                throw runtime_error("La cantidad de impresiones debe ser un número entero positivo");
-            }
-            
-            tieneImpresiones = true;
-            i++;  // Saltar el valor del parámetro
-            
-        } else if (arg == "-a" || arg == "--archivo") {
+        if (arg == "-a" || arg == "--archivo") {
             if (i + 1 >= argc) {
                 throw runtime_error("Falta la ruta del archivo después de " + arg);
             }
@@ -74,10 +58,6 @@ void verificarParametros(int argc, char *argv[]) {
             throw runtime_error("Parámetro desconocido: " + arg);
         }
     }
-
-    if (!tieneImpresiones) {
-        throw runtime_error("Falta especificar la cantidad de impresiones (-i/--impresiones)");
-    }
     
     if (!tieneArchivo) {
         throw runtime_error("Falta especificar el archivo (-a/--archivo)");
@@ -86,11 +66,11 @@ void verificarParametros(int argc, char *argv[]) {
 
 
 int main(int argc, char *argv[]) {
-    if (argc > 1 && strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+    if ((argc > 1 && strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "--help") == 0)) {
         mostrarAyuda();
         return 0;
     }
-    if (argc < 5) {
+    if (argc < 2) {
         cout << "Error: Faltan parámetros requeridos." << endl;
         mostrarAyuda();
         return 1;
@@ -98,8 +78,7 @@ int main(int argc, char *argv[]) {
 
     verificarParametros(argc, argv);
 
-    int cantImpresiones = atoi(argv[2]);
-    string rutaArchivo = argv[4];
+    string rutaArchivo = argv[2];
 
     // Crear FIFO privado antes de enviar la solicitud
     string fifoPrivado = "/tmp/FIFO_" + to_string(getpid());
--- ejercicio3/ejercicio3Servidor.cpp
+++ ejercicio3/ejercicio3Servidor.cpp
@@ -23,6 +23,7 @@ void mostrarAyuda() {
     cout << "  Este es el programa servidor el cual tomara los " << endl;
     cout << "  archivos que le envien los clientes y los imprimira y procesara." << endl;
     cout << "Opciones:" << endl;
+    cout << "   -i / --impresiones <cantidad>  Cantidad de impresiones a procesar. (Requerido)." << endl;
     cout << "  -h                               Muestra este mensaje de ayuda" << endl;
     cout << endl;
     cout << "Ejemplos:" << endl;
@@ -32,9 +33,29 @@ void mostrarAyuda() {
 
 
 int main(int argc, char *argv[]) {
-    if (argc > 1 && strcmp(argv[1], "-h") == 0) {
+    if (argc > 1 && (strcmp(argv[1], "-h") || strcmp(argv[1], "--help")) == 0) {
         mostrarAyuda();
         return 0;
+    }
+
+    if (argc < 2) {
+        cout << "Parámetros incompletos." << endl;
+        mostrarAyuda();
+        return 1;
+    }
+
+    // verificar si -i es nro
+    if (strcmp(argv[1], "-i") == 0 || strcmp(argv[1], "--impresiones") == 0) {
+        string valor = argv[2];
+        if (valor.find_first_not_of("0123456789") != string::npos) {
+                throw runtime_error("La cantidad de impresiones debe ser un número entero positivo");
+        }
+    }
+
+    int cantidadImpresiones = stoi(argv[2]);
+    if (cantidadImpresiones <= 0) {
+        cout << "La cantidad de impresiones debe ser un número entero positivo." << endl;
+        return 1;
     }
 
     unlink(NOMBRE_FIFO);
@@ -74,49 +95,57 @@ int main(int argc, char *argv[]) {
 
     cout << "Servidor de impresión iniciado. Esperando trabajos..." << endl;
 
+    int impresionesRealizadas = 0;
+
+    cout << "Cantidad de impresiones a procesar: " << cantidadImpresiones << endl;
+
     while (true) {
-        char buffer[1024];
-        if (fgets(buffer, sizeof(buffer), fifo_file)) {
-            ultima_actividad = time(nullptr);
-            linea = buffer;
-            
-            size_t sep = linea.find(':');
-            if (sep != string::npos) {
-                string pidCliente = linea.substr(0, sep);
-                string archivo = linea.substr(sep + 1);
-                archivo.erase(remove(archivo.begin(), archivo.end(), '\n'), archivo.end());
+        while(impresionesRealizadas < cantidadImpresiones) {
+            char buffer[1024];
+            if (fgets(buffer, sizeof(buffer), fifo_file)) {
+                ultima_actividad = time(nullptr);
+                linea = buffer;
+                
+                size_t sep = linea.find(':');
+                if (sep != string::npos) {
+                    string pidCliente = linea.substr(0, sep);
+                    string archivo = linea.substr(sep + 1);
+                    archivo.erase(remove(archivo.begin(), archivo.end(), '\n'), archivo.end());
 
-                cout << "Procesando: " << archivo << endl;
-                sleep(2); // Simular impresión
+                    cout << "Procesando: " << archivo << endl;
+                    sleep(2); // Simular impresión
 
-                // Registrar en log
-                ofstream log(LOG_TEMPORAL, ios::app);
-                ifstream archivo_leer(archivo);
-                if (log && archivo_leer) {
-                    auto ahora = chrono::system_clock::now();
-                    time_t t = chrono::system_clock::to_time_t(ahora);
-                    log << "PID " << pidCliente << " | Archivo: " << archivo 
-                        << " | Fecha: " << put_time(localtime(&t), "%F %T") << "\n"
-                        << archivo_leer.rdbuf() << "\n\n";
+                    // Registrar en log
+                    ofstream log(LOG_TEMPORAL, ios::app);
+                    ifstream archivo_leer(archivo);
+                    if (log && archivo_leer) {
+                        auto ahora = chrono::system_clock::now();
+                        time_t t = chrono::system_clock::to_time_t(ahora);
+                        log << "PID " << pidCliente << " | Archivo: " << archivo 
+                            << " | Fecha: " << put_time(localtime(&t), "%F %T") << "\n"
+                            << archivo_leer.rdbuf() << "\n\n";
+                    }
+
+                    // Responder al cliente
+                    ofstream respuesta("/tmp/FIFO_" + pidCliente);
+                    if (respuesta) respuesta << "OK" << endl;
                 }
-
-                // Responder al cliente
-                ofstream respuesta("/tmp/FIFO_" + pidCliente);
-                if (respuesta) respuesta << "OK" << endl;
+            } else {
+                // Verificar timeout
+                if (difftime(time(nullptr), ultima_actividad) > TIMEOUT_SEGUNDOS) {
+                    cout << "Timeout de inactividad. Cerrando servidor..." << endl;
+                    break;
+                }
+                usleep(100000); // Esperar 100ms antes de reintentar
             }
-        } else {
-            // Verificar timeout
-            if (difftime(time(nullptr), ultima_actividad) > TIMEOUT_SEGUNDOS) {
-                cout << "Timeout de inactividad. Cerrando servidor..." << endl;
-                break;
-            }
-            usleep(100000); // Esperar 100ms antes de reintentar
+            impresionesRealizadas++;
         }
-    // Limpieza
-    fclose(fifo_file);
-    close(fd);
-    unlink(NOMBRE_FIFO);
-    cout << "Servidor cerrado correctamente" << endl;
-    return 0;
+
+        // Limpieza
+        fclose(fifo_file);
+        close(fd);
+        unlink(NOMBRE_FIFO);
+        cout << "Servidor cerrado correctamente" << endl;
+        return 0;
     }
 }
